f76c97f39d0a15793b2720e7eab26d39df0775d9
“yfj <15755407393@163.com>
2018-11-04 21:10:26 +0800

    更正的数据的配置

--- mongodb/__init__.py
+++ mongodb/__init__.py
@@ -1,11 +1,11 @@
 import  pymongo
 
 try:
-    uri = 'mongodb://127.0.0.1:27017/pdd'
+    uri = 'mongodb://127.0.0.1:27017/test'
     client=pymongo.MongoClient(uri)
 
     #2.操作的数据库
-    db=client.pdd
+    db=client.test
 
 
 except Exception as  ex:
